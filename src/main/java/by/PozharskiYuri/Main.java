cb1825abc91337003c9ae5caad0f0a9fbc839fe2
Yury Pazharski <Pozharskiyuri@gmail.com>
2025-01-07 17:42:30 +0300

    Initial commit

---
+++ src/main/java/by/PozharskiYuri/Main.java
@@ -0,0 +1,9 @@
+package by.PozharskiYuri;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Nothing happens in main method of " + Main.class.getName() + " class");
+    }
+
+}
