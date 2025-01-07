3ef4d85c0fb2e8b19356831214ec941ef8640e80
Yury Pazharski <Pozharskiyuri@gmail.com>
2025-01-07 18:29:47 +0300

    Add javaDoc for Animal class methods

--- src/main/java/by/PozharskiYuri/Animal.java
+++ src/main/java/by/PozharskiYuri/Animal.java
@@ -8,6 +8,9 @@ public class Animal {
         this.name = name;
     }
 
+    /**
+     * @param distance дистанция бега в метрах.
+     */
     public void run(double distance) {
         if (distance < 0) {
             throw new IllegalArgumentException("Distance can't be negative.");
@@ -16,6 +19,9 @@ public class Animal {
         System.out.printf("%s пробежал(-а) %.2f метров.\n", name, distance);
     }
 
+    /**
+     * @param distance дистанция плавания в метрах.
+     */
     public void swim(double distance) {
         if (distance < 0) {
             throw new IllegalArgumentException("Distance can't be negative.");
