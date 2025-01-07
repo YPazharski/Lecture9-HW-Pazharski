20b00a9006ef6fc5fcf23dc03d21bd3404406394
Yury Pazharski <Pozharskiyuri@gmail.com>
2025-01-07 18:22:39 +0300

    Add distance check for "run" and "swim" methods of Animal class

--- src/main/java/by/PozharskiYuri/Animal.java
+++ src/main/java/by/PozharskiYuri/Animal.java
@@ -9,10 +9,18 @@ public class Animal {
     }
 
     public void run(double distance) {
+        if (distance < 0) {
+            throw new IllegalArgumentException("Distance can't be negative.");
+        }
+
         System.out.printf("%s пробежал(-а) %.2f метров.\n", name, distance);
     }
 
     public void swim(double distance) {
+        if (distance < 0) {
+            throw new IllegalArgumentException("Distance can't be negative.");
+        }
+
         System.out.printf("%s проплыл(-а) %.2f метров.\n", name, distance);
     }
 
