2793e8462bc997d2f79959875773b92584736099
Yury Pazharski <Pozharskiyuri@gmail.com>
2025-01-13 13:06:24 +0300

    Add lesson_10 package with it's Lesson10Main class

---
+++ src/main/java/by/PazharskiYury/lesson_10/Lesson10Main.java
@@ -0,0 +1,11 @@
+package by.PazharskiYury.lesson_10;
+
+import by.PazharskiYury.Main;
+
+public class Lesson10Main {
+
+    public static void main(String[] args) {
+        System.out.println("Nothing happens in main method of " + Lesson10Main.class.getName() + " class");
+    }
+
+}
