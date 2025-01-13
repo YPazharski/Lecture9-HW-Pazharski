be091dd39d53d1a82e6a8b8297241c29dda6741b
Yury Pazharski <Pozharskiyuri@gmail.com>
2025-01-13 13:32:39 +0300

    Написать метод, который меняет два элемента массива местами (массив может быть любого ссылочного типа)

--- src/main/java/by/PazharskiYury/lesson_10/Lesson10Main.java
+++ src/main/java/by/PazharskiYury/lesson_10/Lesson10Main.java
@@ -1,9 +1,16 @@
 package by.PazharskiYury.lesson_10;
 
+import java.util.Arrays;
+
 public class Lesson10Main {
 
     public static void main(String[] args) {
-        System.out.println("Nothing happens in main method of " + Lesson10Main.class.getName() + " class");
+        String[] stringArray = new String[] {"1", "2"};
+        ArrayElementsSwapper.swapArrayElements(stringArray, 0, 1);
+        System.out.println(Arrays.toString(stringArray));
+        Integer[] intArray = new Integer[] {1, 2, 3};
+        ArrayElementsSwapper.swapArrayElements(intArray, 0, 2);
+        System.out.println(Arrays.toString(intArray));
     }
 
 }
