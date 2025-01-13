50ebae883cdc5a6c2dc5751b74ed6270fc7a5f8f
Yury Pazharski <Pozharskiyuri@gmail.com>
2025-01-13 13:45:45 +0300

    Create Orange and Apple classes

---
+++ src/main/java/by/PazharskiYury/lesson_10/Apple.java
@@ -0,0 +1,9 @@
+package by.PazharskiYury.lesson_10;
+
+public class Apple extends Fruit {
+
+    public Apple(double weight) {
+        super(weight);
+    }
+
+}
---
+++ src/main/java/by/PazharskiYury/lesson_10/Orange.java
@@ -0,0 +1,9 @@
+package by.PazharskiYury.lesson_10;
+
+public class Orange extends Fruit {
+
+    public Orange(double weight) {
+        super(weight);
+    }
+
+}
